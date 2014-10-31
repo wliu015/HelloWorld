64dedd0964d7d76accd7507719dcf6ec34ff16f9
wliu015 <wliu015@fiu.edu>
2014-10-31 15:55:47 +0000

    test10

--- test_case/test_HelloWorld.cpp
+++ test_case/test_HelloWorld.cpp
@@ -1,17 +1,20 @@
 #include "../code/HelloWorld.h"
 #include <iostream>
-
+#include <unistd.h>
 int main()
 {
     cout<<"test case 1: test HelloWorld class exsist?"<<endl;
     HelloWorld h;
+    sleep(5);
     cout<<" test case 1 passed!"<<endl;
     
     cout<<"test case 2: test method say exsist?"<<endl;
     string str = h.say();
+    sleep(5);
     cout<<"test case 2 passed!"<<endl;
 
     cout<<"test case 3: test method say return the right value?"<<endl;
+    sleep(5);
     if(str.compare("Hello!") == 0){
         cout<<"test case 3 passed!"<<endl;
     }else{
